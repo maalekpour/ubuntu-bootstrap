127ec1bfc11ed501d09b7a97f905ceb11dc5ea72
ali <ali@ali.com>
2026-02-24 13:33:21 +0330

    bug fixed

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -191,7 +191,7 @@ install_machine_vision() {
 
 install_network() {
     echo -e "${YELLOW}>>> Installing Network Tools...${RESET}"
-    sudo apt install -y wireshark net-tools nmap tcpdum
+    sudo apt install -y wireshark net-tools nmap tcpdump
 }
 
 install_gui_apps() {
